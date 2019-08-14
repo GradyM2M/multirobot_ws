e9123bb3d333a20e20fdf684348547fa60f60a6b
YikangJ <jinyikangjyk@163.com>
2019-08-14 09:56:29 +0800

    init workspace and original source codes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/jyk/multirobot_ws/src
+# Build directory: /home/jyk/multirobot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(multirobot_test)
+subdirs(m-explore/map_merge)
+subdirs(m-explore/explore)
